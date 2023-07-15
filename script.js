e401a2658422b038f8ef2e7c8a9fd7551a5485f0
Imad <imadzaanine@gmail.com>
2023-07-16 00:09:18 +0200

    Created the PlayRound function

--- script.js
+++ script.js
@@ -1,1 +1,49 @@
-console.log("Hello world!");
+function playRound(playerSelection, computerSelection) {
+    while(playerSelection === "rock"){
+    if (computerSelection === "scissors") {
+        return "You have won! the computer choosed scissors"
+    } else {
+        if (computerSelection === "paper") {
+            return "You have lost! the computer choosed paper"
+        } else {
+            if ( computerSelection === "rock") {
+                return "Its a draw! Both of you choosed rock"
+            } else {
+                
+            }
+            
+        }
+        
+    }
+    }
+    while (playerSelection === "paper") {
+        if (computerSelection === "scissors") {
+            return "You have lost! the computer choosed scissors"
+        } else {
+            if (computerSelection === "paper") {
+                return "Its a draw! Both of you choosed paper"
+            } else {
+                if ( computerSelection === "rock") {
+                    return "You have won! the computer choosed rock"
+                } else {
+                    
+                }
+                
+            }
+    }
+    while (playerSelection === "scissors") {
+        if (computerSelection === "scissors") {
+            return "Its a draw! Both of you choosed scissors"
+        } else {
+            if (computerSelection === "paper") {
+                return "You have won! the computer choosed paper"
+            } else {
+                if ( computerSelection === "rock") {
+                    return "You have lost! the computer choosed rock"
+                } else {
+                    
+                }
+                
+            }
+    }
+}
